6627eea8a9aad1680111550d2e329c65c2d2196c
Jack Curtin <82333861+JackCurtn@users.noreply.github.com>
2024-05-06 23:41:45 +0100

    Created a basic view which allows the user to select a car make. Modified the workflow file so jobs are more clear and easier to visualize

---
+++ DoneDealProject/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DoneDealProject.Services;
+using DoneDealProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoneDealProject.ViewModel;
+
+public partial class MainViewModel : BaseViewModel
+{
+    private CarMakesService _carMakesService;
+
+    public ObservableCollection<string> CarMakes { get; private set; }
+
+    public MainViewModel()
+    {
+        Title = "Front Page";
+        _carMakesService = new CarMakesService();
+        CarMakes = new ObservableCollection<string>();
+
+        LoadCarMakes();
+    }
+
+    // Loads the car makes from the DoneDeal website
+    private async void LoadCarMakes()
+    {
+        List<CarMakes> carMakesList = await _carMakesService.GetCarMakesAsync();
+        if (carMakesList != null)
+        {
+            // Distinct as "All Items" seems to duplicate
+            var uniqueCarMakes = carMakesList.Select(carMake => carMake.Name).Distinct();
+            foreach (var carMake in uniqueCarMakes)
+            {
+                CarMakes.Add(carMake);
+            }
+        }
+    }
+}
+
